e921b9a54af76e985302e391360d597d157d9995
natty.ma <natty.ma@vipshop.com>
2018-01-05 10:57:04 +0800

    3

--- src/main/scala/com/pmpa/scala/common/Person.scala
+++ src/main/scala/com/pmpa/scala/common/Person.scala
@@ -2,16 +2,17 @@ package com.pmpa.scala.common
 
 /**
  * Created by natty.ma on 2018/1/4.
- *  (1) 为类创建主构造函数，与java区别较大。Scala类的主构造函数包含：
- *        a.构造函数参数
- *        b.在类内部调用的方法
- *        c.在类内部执行的语句和表达式。
- *      类里边的方法是构造函数的一部分。age声明为var，scala会为它生成访问和修改方法。访问：p.age 修改：p.age=xxx
- *
+ *  (1) 为类创建主构造函数
+ *  (2) 控制构造函数字段的可见性
  */
 
 /**
  * 功能：创建类的主构造函数
+ *      为类创建主构造函数，与java区别较大。Scala类的主构造函数包含：
+ *        a.构造函数参数
+ *        b.在类内部调用的方法
+ *        c.在类内部执行的语句和表达式。
+ *      类里边的方法是构造函数的一部分。age声明为var，scala会为它生成访问和修改方法。访问：p.age 修改：p.age=xxx
  * @param firstName
  * @param lastName
  *    author:natty.ma  2018-01-04
